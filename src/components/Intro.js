07ff61bbf2cdc661fd8ee2aa8643069532c75017
Sergey Khmelevskoy <xs290@me.com>
2017-11-10 01:42:19 +0300

    changed intro, bg color and 200 surge fix

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,6 +4,8 @@ import api from '../constructor/Api'
 import { TweenMax, Back } from 'gsap';
 
 import Topbar from './Topbar';
+import sprite from '../images/sprite.svg';
+import logo from '../images/logo.png'
 
 export default class Intro extends Component {
   constructor() {
@@ -53,17 +55,21 @@ export default class Intro extends Component {
     return (
       <div className="intro">
         <Topbar />
-        <div className="intro__description">
-          <div className="intro__control">
-            Используйте механизм бесконечной прокрутки
+        <div className="intro__wrapper">
+          <div className="intro__logo">
+            <img src={logo} alt="logo"/>
           </div>
-          <div className="intro__control">
-            Смотрите истории интересных людей
+          <div className="intro__description">
+            <div className="intro__control">
+              Используйте механизм бесконечной прокрутки
+            </div>
+            <div className="intro__control">
+              Смотрите истории интересных людей
+            </div>
+            <div className="intro__control">
+              Присылайте свои истории. #ДЕЛИСЬВЗГЛЯДОМ
+            </div>
           </div>
-          <div className="intro__control">
-            Присылайте свои истории. #ДЕЛИСЬВЗГЛЯДОМ
-          </div>
-
         </div>
 
         <div className="intro__cta">
