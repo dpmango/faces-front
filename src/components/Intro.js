f6389f9ea34c6b671ff37c196adc52dcf7d556b8
Sergey Khmelevskoy <xs290@me.com>
2017-11-10 20:33:56 +0300

    patched intro section, animations, backgeound

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../constructor/Api'
-import { TweenMax, Back } from 'gsap';
+import { TweenMax, Back, Elastic } from 'gsap';
 
 import Topbar from './Topbar';
 import sprite from '../images/sprite.svg';
 import logo from '../images/logo.png'
+import background from '../images/starbg.jpg'
 
 export default class Intro extends Component {
   constructor() {
@@ -17,8 +18,12 @@ export default class Intro extends Component {
   }
 
   animateIntro = () => {
-    TweenMax.to('.intro__description', 1, {opacity: 1, delay: 1, ease: Back.easeOut.config(1.7)});
-    TweenMax.to('.intro__cta', 1, {opacity: 1, delay: 3, ease: Back.easeOut.config(1.7)});
+    TweenMax.to('.intro__bg', 13, {scale: 1, delay: 2.5});
+    TweenMax.to('.intro__logo', 1, {opacity: 1, y: 0, delay: 1, ease: Back.easeOut.config(2)});
+    [].forEach.call( document.querySelectorAll('.intro__control'), function(control, index) {
+      TweenMax.to(control, 1, {opacity: 1, y: 0, delay: 2 + (index * 0.25), ease: Elastic.easeOut.config(1, 0.6)})
+    })
+    TweenMax.to('.intro__cta', 1, {opacity: 1, y: 0, delay: 4, ease: Back.easeOut.config(2)});
   }
 
   preloadImages = () => {
@@ -62,15 +67,25 @@ export default class Intro extends Component {
           <div className="intro__description">
             <div className="intro__control">
               Используйте механизм бесконечной прокрутки
+              <svg className="ico ico-i-move">
+                <use xlinkHref={sprite + "#ico-i-move"} />
+              </svg>
             </div>
             <div className="intro__control">
               Смотрите истории интересных людей
+              <svg className="ico ico-i-user">
+                <use xlinkHref={sprite + "#ico-i-user"} />
+              </svg>
             </div>
             <div className="intro__control">
               Присылайте свои истории. #ДЕЛИСЬВЗГЛЯДОМ
+              <svg className="ico ico-i-share">
+                <use xlinkHref={sprite + "#ico-i-share"} />
+              </svg>
             </div>
           </div>
         </div>
+        <img className="intro__bg" src={background} />
 
         <div className="intro__cta">
           <Link className="btn btn-line" to='/grid'>
